960968951b84ff8637ebe6f77535432a825a9dd8
Danilo Thiago <danilo.t.a.oliveira@hotmail.com>
2017-11-16 13:55:03 -0300

    topo index

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-index',
@@ -8,9 +9,15 @@ import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
-  constructor() { }
+  public constructor(private titleService: Title) { }
+
+  public setTitle(newTitle: string) {
+    this.titleService.setTitle(newTitle);
+  }
 
   ngOnInit() {
+    this.setTitle('Wilivro')
   }
+
 
 }
