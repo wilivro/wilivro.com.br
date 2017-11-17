0a9f18bea23da6768f315e70a6a2cf68bbedb054
Danilo Thiago <danilo.t.a.oliveira@hotmail.com>
2017-11-17 15:12:55 -0300

    resultados

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Element } from '@angular/compiler';
+declare var $: any;
+declare var mask: any;
+var on = true;
 
 @Component({
   selector: 'app-index',
@@ -16,8 +20,49 @@ export class IndexComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.setTitle('Wilivro')
+    this.setTitle('Wilivro');
+    this.resultados();
   }
+
+  contador(el: any, inicial: number, total: number, speed: number, jump: number) {
+    console.log(el, inicial, total, speed)
+    setInterval(function () {
+      if (inicial <= total) {
+        el.innerHTML = inicial;
+      }
+      inicial = inicial + jump;
+    }, speed);
+
+  }
+
+
+
+  private resultados() {
+    var _this = this;
+    $(document).scroll(function () {
+
+      if (!on) return;
+
+      if (window.scrollY > $('#resultados').offset().top - window.innerHeight / 2) {
+
+        $('span.cont').map(function () {
+          var inicial = $(this).attr('data-inicial');
+          var total = $(this).attr('data-total');
+          var speed = $(this).attr('data-speed');
+          var jump = $(this).attr('data-jump');
+          this.mask('000.000.000.000.000', { reverse: true })
+          _this.contador(this, parseInt(inicial), parseInt(total), parseInt(speed), parseInt(jump));
+        })
+
+        on = false;
+
+      }
+
+
+    })
+  }
+
+
 
 
 }
