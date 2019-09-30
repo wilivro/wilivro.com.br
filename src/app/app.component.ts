655874bb984400b8c809c760aaf8ba8e1e7090a4
adensoares <adensoares@gmail.com>
2019-09-30 15:50:31 -0300

    fix 404 error - 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Wilivro';
+
+  constructor(private router: Router) {
+    let path = localStorage.getItem('path');
+    if(path) {
+      localStorage.removeItem('path');
+      this.router.navigate([path]);
+    }
+  }
 }
